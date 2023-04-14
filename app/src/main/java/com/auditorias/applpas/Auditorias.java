4d7d03699282979ed264de30784d214485310bbd
gvillanuevap <gvillanuevap@enerya.com>
2023-04-14 17:04:53 -0600

    trabajando en diseño para mostrar las auditorias

--- app/src/main/java/com/auditorias/applpas/Auditorias.java
+++ app/src/main/java/com/auditorias/applpas/Auditorias.java
@@ -1,6 +1,7 @@
 package com.auditorias.applpas;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -10,8 +11,42 @@ public class Auditorias extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auditorias);
         TextView titulo_toolbar =(TextView)findViewById(R.id.titulo_toolbar);
         titulo_toolbar.setText("Auditorías");
+
+        // Obtenemos el LinearLayout padre
+        LinearLayout linearLayoutPadre = findViewById(R.id.layoutPrincipal);
+
+
+        // Creamos varios LinearLayouts hijos
+        for (int i = 0; i < 5; i++) {
+            // Creamos el LinearLayout hijo
+            LinearLayout linearLayoutHijo = new LinearLayout(this);
+            linearLayoutHijo.setOrientation(LinearLayout.VERTICAL);
+
+
+            // Agregamos el fondo drawable al LinearLayout hijo
+            linearLayoutHijo.setBackground(getResources().getDrawable(R.drawable.fondo_lista_auditorias));
+
+            // Creamos las cuatro líneas de texto
+            for (int j = 0; j < 4; j++) {
+                TextView textView = new TextView(this);
+                textView.setText("Texto de la línea " + (j + 1));
+                linearLayoutHijo.addView(textView);
+            }
+
+
+            // Creamos un TextView de separación
+            TextView separador = new TextView(this);
+            separador.setLayoutParams(new LinearLayout.LayoutParams(5, LinearLayout.LayoutParams.MATCH_PARENT));
+
+
+            // Agregamos el LinearLayout hijo al LinearLayout padre
+            linearLayoutPadre.addView(linearLayoutHijo);
+            linearLayoutPadre.addView(separador);
+        }
+
 
 
     }
--- app/src/main/java/com/auditorias/applpas/Login.java
+++ app/src/main/java/com/auditorias/applpas/Login.java
@@ -2,13 +2,21 @@ package com.auditorias.applpas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,10 +26,25 @@ import java.util.Map;
 
 public class Login extends AppCompatActivity {
 
+    RequestQueue requestQueue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button btnIngresar = (Button)findViewById(R.id.btnIngresar);
+        EditText editNomina = (EditText)findViewById(R.id.nomina);
+        EditText editClave = (EditText)findViewById(R.id.clave);
+
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nomina = editNomina.getText().toString();
+                String clave = editClave.getText().toString();
+                verificarLogin(nomina,clave);
+            }
+        });
 
 
 
@@ -29,19 +52,47 @@ public class Login extends AppCompatActivity {
     }
 
 
-    /*String url = "https://vvnorth.com/lpa/app/verificar.php";
-    StringRequest request = new StringRequest(Request.Method.POST, url,
-            response -> {
-                Toast.makeText(getApplicationContext(), "Recibiendo",Toast.LENGTH_SHORT).show();
-            }, error -> {
-                Toast.makeText(getApplicationContext(), "Recibiendo",Toast.LENGTH_SHORT).show();
-            }) {
-        @Override
-        protected Map<String, String> getParams() {
-            Map<String, String> params = new HashMap<>();
-            params.put("correo", "usuario@dominio.com");
-            params.put("contrasena", "123456");
-            return params;
+    //METODO PARA VERIFICAR SI EXISTE EL USUARIO
+            private void verificarLogin(String nomina, String clave){
+                String url = "https://vvnorth.com/lpa/app/verificar.php";
+                StringRequest request = new StringRequest(Request.Method.POST, url,
+                        response -> {
+                            Log.e("RESPUESTA",""+response);
+                            try {
+                                JSONObject jsonResponse = new JSONObject(response);
+                                String id_auditor = jsonResponse.getString("id_auditor");
+                                String nombre_auditor = jsonResponse.getString("auditor");
+                                enviarVistaAditorias(id_auditor,nombre_auditor);
+
+
+                            } catch (JSONException e) {
+                                Toast toast = Toast.makeText(getApplicationContext(),"No se encontro Usuario, verifique.",Toast.LENGTH_LONG);
+                                toast.show();
+                            }
+
+
+                        }, error -> {
+                            Toast.makeText(getApplicationContext(), "Error :-(", Toast.LENGTH_SHORT).show();
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("Usuario", nomina);
+                        params.put("Contrasena", clave);
+                        return params;
+                    }
+                };
+
+                requestQueue = Volley.newRequestQueue(this);
+                requestQueue.add(request);
+            }
+
+
+    //METODO PARA ENVIARTE A LA PANTALL CORRESPONDIENTE SEGUN TIPO USUARIO
+            private void enviarVistaAditorias(String id_auditor, String nombre_auditor){
+                Intent intent = new Intent(Login.this, Auditorias.class);
+                intent.putExtra("ID_AUDITOR", id_auditor);
+                intent.putExtra("NOMBRE_AUDITOR", nombre_auditor);
+                startActivity(intent);
+            }
         }
-    };*/
-}
